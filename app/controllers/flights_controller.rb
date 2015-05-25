8e3f58d6a62894a503765c1d74f16d1817891ef6
CollinSmith <collinsmith1990@gmail.com>
2015-05-25 17:36:32 -0600

    Added flight selection form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,6 +7,10 @@ class FlightsController < ApplicationController
     @date_options = Flight.date_options
     #Results
     unless params[:from_airport_id] == nil
+      @selected_from_airport = params[:from_airport_id]
+      @selected_to_airport = params[:to_airport_id]
+      @selected_passengers = params[:passengers]
+      @selected_date = params[:date]
       @flights = Flight.search_flights(params[:from_airport_id], params[:to_airport_id], params[:date])
     end
   end
