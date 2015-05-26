05862edc522106f3f9fac892f02c37db3c397aef
CollinSmith <collinsmith1990@gmail.com>
2015-05-25 21:18:23 -0600

    Added bookings show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,12 +8,15 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    byebug
     if @booking.save
-      redirect_to root_url
+      redirect_to booking_path(@booking)
     else
       render 'new'
     end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
